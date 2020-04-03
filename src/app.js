982de35202bd1f6664b33f93391a2a286b55e14b
Olivia Davis <olivia.davis593@gmail.com>
2020-04-03 16:31:07 -0500

    intital commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config') 
+const ArticlesSerivce = require('./articles-service')
 
 const app = express()
 
@@ -15,9 +16,22 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    //we want to use ArticlesService.getAllArticles inside endpoint => to populate response
+   ArticlesSerivce.getAllArticles(knexInstance)
+    .then(articles => {
+        res.json(articles)
+    })
+     //passing next into .catch from promise chain so that any error get 
+     //handled by our error handler middleware
+    .catch(next)
+})
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
+
 
 app.use((error, req, res, next) => {
     let response
